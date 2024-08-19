b25ece84e8468a98e7f63ee0b4bdcd689911b97e
junn216 <junbeomb216@gmail.com>
2024-08-19 22:54:49 +0900

    Add LabProject04 : switch full window screen

--- LabProject04/GameFramework.h
+++ LabProject04/GameFramework.h
@@ -6,7 +6,7 @@ class GameFramework
 {
 private:
 	//다음은 게임 프레임워크에서 사용할 타이머이다.
-	CGameTimer m_GameTimerr;
+	CGameTimer m_GameTimer;
 	//다음은 프레임 레이트를 주 윈도우의 캡션에 출력하기 위한 문자열이다.
 	_TCHAR m_pszFrameRate[50];
 
@@ -79,5 +79,6 @@ public:
 	LRESULT CALLBACK OnProcessingWindowMessage(HWND hWnd, UINT nMessageID, WPARAM wParam,
 		LPARAM lParam);
 	//윈도우의 메시지(키보드, 마우스 입력)를 처리하는 함수이다.
+	void ChangeSwapChainState();
 };
 
